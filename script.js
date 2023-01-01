631f90cc63d45e4cde3dea95f404b60dbda580ff
Ascensionbanks <78037864+Ascensionbanks@users.noreply.github.com>
2023-01-01 15:44:05 -0500

    WFWFFWFWF

--- script.js
+++ script.js
@@ -52,9 +52,9 @@ function getCookie(cname) {
 }
 
 function checkCookie() {
-  let user = getCookie("username");
+  let user = getCookie("username"); 
   if (user != "") {
-    alert("Welcome again " + user);
+    alert("Welcome again " + user + "!!!!!!!!!!!!!!");
   } else {
      user = prompt("Please enter your name:","");
      if (user != "" && user != null) {
